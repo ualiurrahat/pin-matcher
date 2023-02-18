5c563500c615230846dcab7981c8cfa2e4923bf2
ualiurrahat <ualiurrahat25@gmail.com>
2023-02-18 12:36:53 +0600

    added event bubble to use calculator,clear button

--- js/app.js
+++ js/app.js
@@ -7,13 +7,44 @@ function generatePin() {
 function getPin() {
     const pin = generatePin();
     const pinString = pin + '';
-    if(pinString.length === 4)
-    {
+    if (pinString.length === 4) {
         return pin;
     }
-    else
-    {
+    else {
         // pin is not a valid one. generate pin gain
         return getPin();
     }
 }
+// task : show 4 digit pin on the left side pin generator display input
+// 1.add event listener to the generate pin button
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+    // 2. get display-pin
+    const displayPin = document.getElementById('display-pin');
+    // 3. set display-pin value
+    displayPin.value = pin;
+});
+
+// event delegate to all calculator button
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+
+    else {
+        const newTypedNumber = number + previousTypedNumber;
+        typedNumberField.value = newTypedNumber;
+
+    }
+});
